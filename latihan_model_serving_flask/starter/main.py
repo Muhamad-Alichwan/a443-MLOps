ea83346008daea5bba8f13693e993596432919dc
Muhamad Alichwan <alichwan.muhamad@gmail.com>
2024-03-29 14:30:30 +0700

    add new code

--- latihan_model_serving_flask/starter/main.py
+++ latihan_model_serving_flask/starter/main.py
@@ -6,6 +6,14 @@ from flask import Flask, request
 
 #================Membuat Web App Sederhana Menggunakan Flask========================
 # Your code here
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
 #===================================================================================
 
 
